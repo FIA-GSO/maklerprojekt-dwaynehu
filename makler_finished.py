2febca3ebe26b84cd1e4354bf56f30b02164e30f
dwaynehu <119108789+dwaynehu@users.noreply.github.com>
2023-02-03 12:03:58 +0100

    Update makler_finished.py

--- makler_finished.py
+++ makler_finished.py
@@ -9,32 +9,32 @@ while add_room == "y":
     # ERROR Handling: input validation for float
     while True:
         try:
-            room_dim_a = float(input(f"Enter dimension a of room {room_number}:"))
-            room_dim_b = float(input(f"Enter dimension b of room {room_number}:"))
+            room_dim_a = float(input(f"Enter dimension a of room {room_number}: "))
+            room_dim_b = float(input(f"Enter dimension b of room {room_number}: "))
             break
         except ValueError:
             print("Please use numbers. Try again!")
     # calculate room size and add room size to total_size
     room_size = room_dim_a * room_dim_b
     total_size = total_size + room_size
-    room_name=input(f"give the room a name:")
+    room_name=input(f"give the room a name: ")
     all_rooms_dict[room_name] = room_size
-    obstruction = input("obstruction? (y/n)")
+    obstruction = input("Does the room have an annex? (y/n) ")
     if obstruction[0] == "y":
         while True:
             try:
-                obstruction_dim_a = float(input("Please enter the dimension a for the obstruction: "))
-                obstruction_dim_b = float(input("Please enter the dimension b for the obstruction: "))
+                obstruction_dim_a = float(input("Please enter the dimension a for the annex: "))
+                obstruction_dim_b = float(input("Please enter the dimension b for the annex: "))
                 break
             except ValueError:
                 print("Please use numbers. Try again!")
         obstruction_dim = obstruction_dim_a * obstruction_dim_b
         total_size = total_size + obstruction_dim
-        add_room = input("Do you want to add more rooms? (y/n)")
+        add_room = input("Do you want to add more rooms? (y/n) ")
         room_number = room_number + 1 # increase counter for room number
     # user chose no obstruction
     elif obstruction[0] == "n":
-        add_room = input("Do you want to add more rooms? (y/n)")
+        add_room = input("Do you want to add more rooms? (y/n) ")
         room_number = room_number + 1 # increase counter for room number
     else:  # Output if user did not type yes or no
         print("Type yes or no. Try again!")
